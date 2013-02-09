262726649f821e916eb325d9a3c545cefdd57417
Katja Graefenhain <katja.graefenhain@switch.ch>
2013-02-09 12:39:10 +0100

    Finished first cut of the User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121026130645) do
-
-  create_table "microposts", :force => true do |t|
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 20130209112246) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -26,5 +19,7 @@ ActiveRecord::Schema.define(:version => 20121026130645) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
