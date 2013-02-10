d3ccbb65862d32bc45c6ea95b66cdcafe26e0985
Katja Graefenhain <katja.graefenhain@switch.ch>
2013-02-10 16:36:17 +0100

    User signup complete

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ DemoApp::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  # see http://stackoverflow.com/questions/7443536/blueprint-screen-css-isnt-precompiled
   config.assets.precompile += %w( *.css *.js )
 
   # Disable delivery errors, bad email addresses will be ignored
